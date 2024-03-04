a7f494bbf054512172993ab734b9655abe0c7d36
Brendon45 <jejebrendon722@gmail.com>
2024-03-04 14:44:33 -0800

    full pyramid

--- practices03/full_pyramid01.py
+++ practices03/full_pyramid01.py
@@ -1,6 +1,7 @@
 # Question: Write a program to print the full pyramid of stars
 rows = int(input("Enter the number of rows: "))
 
+
 for i in range(1, rows + 1):
     for j in range(0, rows - i):
         print(" ", end='')
