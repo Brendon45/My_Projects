ffcba66b03693b015df8b77386423b0730c034cd
Brendon45 <jejebrendon722@gmail.com>
2024-03-14 22:05:15 +0200

    second day of 100daysofalxse

--- 0x00-basic_projects/calculator_age.py
+++ 0x00-basic_projects/calculator_age.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 
-#to get cuurent year
+# Importing the datetime module to get the current year
 import datetime
 
-# Ask for details
+# Ask the user for their date of birth details
 year_of_birth = input("Year of birth: ")
-month_of_birth = input("Month of birth (0-12): ")
+month_of_birth = input("Month of birth (1-12): ")
 day_of_birth = input("Day of the month: ")
 
-# current year
+# Get the current year
 current_year = datetime.datetime.now().year
 
-# Display details
+# Display the date of birth
 print("Your date of birth is:", end=" ")
 print(day_of_birth, month_of_birth, year_of_birth, sep="-")
 
 # Calculate the age
 age = current_year - int(year_of_birth)
 
-# Display age
-print("You are ", age, "years old.")
+# Display the age
+print("You are", age, "years old.")
