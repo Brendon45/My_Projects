3af6ef7f4ce5878275dbf535fa0d221e275fd62a
Brendon45 <jejebrendon722@gmail.com>
2024-03-21 06:56:49 -0700

    area of a triangle

---
+++ practices03/area_of_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+## Program to accept the base and height from user 
+## and use them to find the area of a right angle triangle
+
+base = float(input("Enter the base: "))
+height = float(input("Enter the height: "))
+area = (base * height) / 2
+print(f"The area is {area}")
