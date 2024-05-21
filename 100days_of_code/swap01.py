59f2957dfd1039a770153640b4737e23d6b2b456
Brendon45 <jejebrendon722@gmail.com>
2024-05-21 11:48:52 +0200

    swapping values using traditional approach

---
+++ 100days_of_code/swap01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+
+# Prompt user to enter the values of a and b
+a = input("Enter value of a: ")
+b = input("Enter value of b: ")
+
+# Swapping values using multiple assignment
+a, b = b, a
+
+# Printing the values after swapping
+print(f"a = {a}")
+print(f"b = {b}")
+# Prompt user to enter the values of a and b
+a = input("Enter value of a: ")
+b = input("Enter value of b: ")
+
+# Swapping values using multiple assignment
+a, b = b, a
+
+# Printing the values after swapping
+print(f"a = {a}")
+print(f"b = {b}")
