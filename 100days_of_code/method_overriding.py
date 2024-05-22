8e3cc60c2989aef1cceccecbca38b1a0ffab1695
Brendon45 <jejebrendon722@gmail.com>
2024-05-22 12:18:24 +0200

    method overriding

---
+++ 100days_of_code/method_overriding.py
@@ -0,0 +1,15 @@
+class A:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+  
+class B:
+    def add(self, a, b):
+        return a + b
+
+a = B()
+# Method overriding
+print(a.add(3, 4))  # Output: 7
