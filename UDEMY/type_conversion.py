d03782d9bab27751d21a50274bff399f382dc5d0
Brendon45 <jejebrendon722@gmail.com>
2024-05-24 10:25:31 +0200

    type conversion

--- UDEMY/type_conversion.py
+++ UDEMY/type_conversion.py
@@ -33,7 +33,8 @@ print("Data type of num_str before Type Casting:", type(num_str))
 num_str = int(num_str)  # Converting string to integer
 print("Data type of num_str after Type Casting:", type(num_str))
 
-num_sum = num_int + num_str
+
+num_sum = num_int + num_str # prints the data type of num_str after type casting
 
 print("Sum of num_int and num_str:", num_sum)
 print("Data type of the sum:", type(num_sum))
