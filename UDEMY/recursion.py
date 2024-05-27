9bc509977cc0d6ff0dcc5f2096194c4308f04e95
Brendon45 <jejebrendon722@gmail.com>
2024-05-27 12:41:41 +0200

    recursion-factotrial of a number

--- UDEMY/recursion.py
+++ UDEMY/recursion.py
@@ -1,8 +1,11 @@
 # Tutorial 41-Python Recursion
 
+# Define a recursive function to calculate the factorial of a number
 def fact(n):
+    # Base case: if n is 1 or less, the factorial is 1
     if n <= 1:
         return 1
+    # Recursive case: n multiplied by the factorial of (n-1)
     return n * fact(n-1)
-
+# Print the factorial of 5 using the recursive function
 print("Factorial of 5 is", fact(5))
