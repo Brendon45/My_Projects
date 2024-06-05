3d328a7914900e5561458e3d8fb1535601bd4078
Brendon45 <jejebrendon722@gmail.com>
2024-06-05 07:51:48 +0200

    simple concatenate

---
+++ UDEMY_PROJECTS/simple_concatenate.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def concat_uppercase(str1, str2):
+
+    result = str1 + str2
+    return result.upper()
+
+concatenated_str = concat_uppercase(str1="hello ", str2="brendon")
+print(concatenated_str)
+
+    
