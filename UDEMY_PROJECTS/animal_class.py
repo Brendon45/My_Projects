d873dfbc5afa5b0596a1289032a1bb1668a02d17
Brendon45 <jejebrendon722@gmail.com>
2024-06-08 10:19:48 +0200

    Initial commit

---
+++ UDEMY_PROJECTS/animal_class.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self,name):
+        self.name = name
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def make_sound(self):
+        print(f"{self.name} makes a sound.")
+
+    def walk(self):
+        print(f"{self.name} is walking.")
+
+    def run(self):
+        print(f"{self.name} is running.")
+
+class Dog(Animal):
+    def make_sound(self):
+        print(f"{self.name} bark: Woof!")
+
+    def run(self):
+        print(f"{self.name} is running happily")
