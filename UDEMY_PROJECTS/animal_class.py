daea1b3ccb03bad633a1eb62b074ac233e4e6a8a
Brendon45 <jejebrendon722@gmail.com>
2024-06-08 17:08:49 +0200

    inheritance and polymorphism

--- UDEMY_PROJECTS/animal_class.py
+++ UDEMY_PROJECTS/animal_class.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 class Animal:
     def __init__(self,name):
         self.name = name
@@ -20,3 +22,25 @@ class Dog(Animal):
 
     def run(self):
         print(f"{self.name} is running happily")
+
+class Cat(Animal):
+    def make_sound(self):
+        print(f"{self.name} meows")
+    
+    def walk(self):
+        print(f"{self.name} is walking gracefully")
+#Creating instances
+buddy = Dog("Buddy")
+whiskers = Cat("Whiskers")
+
+# Dog behaviors
+buddy.eat()
+buddy.make_sound()
+buddy.walk()
+buddy.run()
+
+# Cat behaviors
+whiskers.eat()
+whiskers.make_sound()
+whiskers.walk()
+whiskers.run()
